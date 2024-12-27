4629688ced0fcebcbb1df3d6fa5cfead551946c9
xComputer-x <adam.kiliszek.10@gmail.com>
2024-12-27 13:42:14 +0100

    Server.py zmiany

--- src/server/server.py
+++ src/server/server.py
@@ -1,6 +1,11 @@
 import socket
 import threading
 
+class Color:
+	error = "\033[38:5:160m"
+	normal = "\033[38:5:7m"
+	info = "\033[38:5:27m"
+
 class Server():
 	def __init__(self, host: str, port: int = 5000, directory: str = None) -> None:
 		self.host = host
@@ -9,22 +14,26 @@ class Server():
 		self.server_socket = socket.create_server((self.host, self.port))
 	
 	def run(self) -> None:
-		print(f"Server is running {self.host}:{self.port}")
+		print(f"{Color.info}INFO:{Color.normal} Server is running {self.host}:{self.port}")
 		while True:
 			client_socket, client_address = self.server_socket.accept()
 			thread = threading.Thread(target = self.handle_request, args = (client_socket, client_address))
 			thread.start()
 	
-	def handle_request(self, client_socket, client_address):
+	def handle_request(self, client_socket, client_address) -> str:
 		request_line = client_socket.recv(1024).decode()
+		if request_line[0:4] != "POST":
+			print(f"{Color.error}ERROR:{Color.normal} invalid request send")
+			return ""
+		
 		request_content = request_line.split('\r\n')
-		headers = {}
-		for i, content in enumerate(request_content):
-			if i == 0:
-				request_type, url, http_version = content.split(" ")
-			elif i == len(request_content) - 1:
-				body = content
-			elif content and ':' in content:
-				key, val = content.split(": ")
-				headers[key] = val
-		print(f"Request content: {request_content}")
+		file_content = ""
+		
+		creating_file = False
+		for i in request_content:
+			if i == '' and not creating_file:
+				creating_file = True
+			
+			if creating_file:
+				file_content += i
+		print(file_content)
