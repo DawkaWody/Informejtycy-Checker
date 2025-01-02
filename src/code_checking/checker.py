953bc5beb0c59240a6c593c556f5faadead6423a
xComputer-x <adam.kiliszek.10@gmail.com>
2025-01-02 14:12:41 +0100

    Zmainki
    Naprawiono działanie sprawdzarki

--- src/code_checking/checker.py
+++ src/code_checking/checker.py
@@ -7,40 +7,40 @@ from .pack_loader import PackLoader
 
 
 class Checker:
-    def __init__(self, compiler: Compiler, pack_loader: PackLoader):
-        self.compiler = compiler
-        self.pack_loader = pack_loader
+	def __init__(self, compiler: Compiler, pack_loader: PackLoader):
+		self.compiler = compiler
+		self.pack_loader = pack_loader
 
-        self.compiled_dir = self.compiler.output_dir
+		self.compiled_dir = self.compiler.output_dir
 
-        self.check_queue = []
+		self.check_queue = []
 
-    def push_check(self, filename: str, ex_id: int, on_checked_func: Callable[[dict], None]) -> None:
-        self.check_queue.append((filename, ex_id, on_checked_func))
+	def push_check(self, filename: str, ex_id: int, on_checked_func: Callable[[dict], None]) -> None:
+		self.check_queue.append((filename, ex_id, on_checked_func))
 
-    def listen(self) -> None:
-        while True:
-            if len(self.check_queue) > 0:
-                filename, ex_id, on_checked = self.check_queue[0]
-                result = self.check(filename, ex_id)
-                on_checked(result)
-                del self.check_queue[0]
+	def listen(self) -> None:
+		while True:
+			if len(self.check_queue) > 0:
+				filename, ex_id, on_checked = self.check_queue[0]
+				result = self.check(filename, ex_id)
+				on_checked(result)
+				del self.check_queue[0]
 
-    def check(self, code_file: str, ex_id: int) -> dict[str, Any]:
-        result = {"%": None, "first_failed": None}
-        score = 0
-        program = self.compiler.compile(code_file)
-        test_pack = self.pack_loader.load_bytes(ex_id)
-        for test_in, test_out in test_pack:
-            data, write = os.pipe()
-            os.write(write, test_in)
-            print(os.path.join(self.compiled_dir, program))
-            o = subprocess.check_output(os.path.join(self.compiled_dir, program), stdin=data, shell=True)
-            if o == test_out:
-                score += 1
-            else:
-                result["first_failed"] = test_in
-                break
+	def check(self, code_file: str, ex_id: int) -> dict[str, Any]:
+		result = {"%": None, "first_failed": None}
+		score = 0
+		program = self.compiler.compile(code_file)
+		test_pack = self.pack_loader.load_bytes(ex_id)
+		for test_in, test_out in test_pack:
+			data, write = os.pipe()
+			os.write(write, test_in)
+			#print(os.path.join(self.compiled_dir, program))
+			o = subprocess.check_output('"'+os.path.join(self.compiled_dir, program)+'"', input=test_in, shell=True)
+			if o.decode()[:-1] == test_out.decode():
+				score += 1
+			else:
+				result["first_failed"] = test_in
+				break
 
-        result["%"] = score / len(test_pack)
-        return result
+		result["%"] = score / len(test_pack)
+		return result
--- src/code_checking/commands.py
+++ src/code_checking/commands.py
@@ -1,6 +1,6 @@
 import subprocess
 from os.path import join
-
+import os
 
 class Compiler:
     def __init__(self, compiler: str, input_dir: str, output_dir: str):
@@ -10,6 +10,6 @@ class Compiler:
 
     def compile(self, filename: str) -> str:
         target_filename = filename[:-3] + 'out'  # file.cpp -> file.out
-        command = self.compiler + ' ' + join(self.input_dir, filename) + ' -o ' + join(self.output_dir, target_filename)
+        command = self.compiler + ' "' + join(self.input_dir, filename) + '" -o "' + join(self.output_dir, target_filename) + '"'
         subprocess.run(command, shell=True)
         return target_filename
