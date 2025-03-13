293fb5c7c5d8703fad7eee59ed89deb4f439b98a
DawkaWody <dawkawody@wp.pl>
2025-03-13 15:29:07 +0100

    przywrócenie kolorków!!!

--- src/logger/colors.py
+++ src/logger/colors.py
@@ -1,19 +1,16 @@
-import sys
-use_colors = sys.stdout.isatty()
-
 class Color:
-    NORMAL = "\033[0m" if use_colors else ""
-    FROM = "\033[38:5:247m" if use_colors else ""
+    NORMAL = "\033[0m"
+    FROM = "\033[38:5:247m"
 
-    SPAM = "\033[38:5:70m" if use_colors else ""
+    SPAM = "\033[38:5:70m"
     SPAM_ID = 0
-    DEBUG = "\033[38:5:207m" if use_colors else ""
+    DEBUG = "\033[38:5:207m"
     DEBUG_ID = 1
-    WARNING = "\033[38:5:99m" if use_colors else ""
+    WARNING = "\033[38:5:99m"
     WARNING_ID = 2
-    ALERT = "\033[38:5:214m" if use_colors else ""
+    ALERT = "\033[38:5:214m"
     ALERT_ID = 3
-    ERROR = "\033[38:5:160m" if use_colors else ""
+    ERROR = "\033[38:5:160m"
     ERROR_ID = 4
-    INFO = "\033[38:5:27m" if use_colors else ""
+    INFO = "\033[38:5:27m"
     INFO_ID = 5
